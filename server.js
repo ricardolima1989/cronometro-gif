63aa64c1df1b86b13f9194cef5a778978e37d4d0
Rica Lima <49537463+ricardolima1989@users.noreply.github.com>
2025-04-07 12:41:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/api/gerar-gif', async (req, res) => {
     encoder.start();
     encoder.setRepeat(0);            // loop infinito
     encoder.setDelay(1000 / 30);      // 250ms por frame (4 FPS)
-    encoder.setQuality(1);           // 1‑30 (1 = melhor)
+    encoder.setQuality(10);           // 1‑30 (1 = melhor)
 
     /* --- canvas buffer --- */
     const canvas = createCanvas(width, height);
