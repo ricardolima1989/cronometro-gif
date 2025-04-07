715e0fd4d4b19fa55414ee0a5b585772c2eec75e
Rica Lima <49537463+ricardolima1989@users.noreply.github.com>
2025-04-07 17:22:07 -0300

    fgdfgd

--- server.js
+++ server.js
@@ -74,5 +74,5 @@ app.post('/api/gerar-gif', async (req, res) => {
 /* =================================== */
 
 app.listen(PORT, () =>
-  console.log(Servidor rodando em http://localhost:${PORT})
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 );
